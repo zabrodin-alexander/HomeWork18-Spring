2cc26cb6d51d3def9907a41f2f845e746153da7b
Zabro <zabrodin.alexander51@gmail.com>
2025-01-02 06:32:30 +0300

    а

---
+++ src/main/java/com/example/Employees/model/Employee.java
@@ -0,0 +1,59 @@
+package com.example.Employees.model;
+
+import java.util.Objects;
+
+public class Employee {
+    private final String firstName;
+    private final String lastName;
+    private int salary;
+    private String department;
+
+    public Employee(String firstName, String lastName, int salary, String department) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                ", department='" + department + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return salary == employee.salary && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(department, employee.department);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, salary, department);
+    }
+}
+
+
+
